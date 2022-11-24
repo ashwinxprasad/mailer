8ca5efeb286a44a90aff47a2a1d94ed0fc92774d
Ashwin Prasad <ashwin@onpar.in>
2022-11-24 12:48:43 +0530

    nodemailer

--- pages/api/request-mailer.ts
+++ pages/api/request-mailer.ts
@@ -2,53 +2,92 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import airtable from "airtable";
 import axios from "axios";
+import nodemailer from "nodemailer";
 import { createHtml } from '../data/email-template-1';
 
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
+        req: NextApiRequest,
+        res: NextApiResponse
 ) {
-  console.log(req.body);
-  if (!req.body.base?.id ?? true) {
-    return {
-      "error": "Invalid Request"
-    }
-  }
-  const base = airtable.base(req.body.base.id as string);
-  const table = base(process.env.TABLE_NAME as string);
+        if (!req.body.base?.id ?? true) {
+                return {
+                        "error": "Invalid Request"
+                }
+        }
+        const base = airtable.base(req.body.base.id as string);
+        const table = base(process.env.TABLE_NAME as string);
 
-  // const record = await table.find("")
-  //
+        // const record = await table.find("")
+        //
 
-  try {
+        try {
 
-    const payload = await axios.get(`https://api.airtable.com/v0/bases/${req.body.base.id}/webhooks/${req.body.webhook.id}/payloads`, {
-      headers: {
-        Authorization: `Bearer ${process.env.BEARER_TOKEN}`
-      }
-    });
+                const payload = await axios.get(`https://api.airtable.com/v0/bases/${req.body.base.id}/webhooks/${req.body.webhook.id}/payloads`, {
+                        headers: {
+                                Authorization: `Bearer ${process.env.BEARER_TOKEN}`
+                        }
+                });
 
-    const index = payload.data.cursor - 2;
-    const tableId = process.env.TABLE_ID;
-    const recordId = (Object.keys(payload.data.payloads[index]['changedTablesById'][tableId!]['changedRecordsById'])[0])
-    console.log({ recordId })
+                const index = payload.data.cursor - 2;
+                const tableId = process.env.TABLE_ID;
+                const recordId = (Object.keys(payload.data.payloads[index]['changedTablesById'][tableId!]['changedRecordsById'])[0])
 
-    const record = await (await table.find(recordId)).fetch();
-    const fields = record.fields;
-    const email = createHtml({
-      title: fields['Article Title'] as string,
-      primaryButton: fields['Article Title'] as string,
-      secondaryBody: fields['Secondary Body'] as string,
-      primaryBody: fields['Primary Body'] as string,
-      articleUrl: fields['Article Link'] as string
-    })
-    res.status(200).send(email);
-  }
+                const record = await (await table.find(recordId)).fetch();
+                const fields = record.fields;
+                const email = createHtml({
+                        title: fields['Article Title'] as string,
+                        primaryButton: fields['Article Title'] as string,
+                        secondaryBody: fields['Secondary Body'] as string,
+                        primaryBody: fields['Primary Body'] as string,
+                        articleUrl: fields['Article Link'] as string
+                })
 
-  catch (e) {
-    console.log(e)
-    res.status(200).json({ name: 'Oops Doe' })
-  }
+
+                const userData = await base('Testing Users')
+                        .select({
+                                view: "Grid view",
+                        })
+                        .all();
+
+                const users: string[] = [];
+                for (const user of userData) {
+                        users.push(user.get("Email") as string);
+                }
+
+                let transporter = nodemailer.createTransport({
+                        host: process.env.SMTP_HOST,
+                        port: 587,
+                        secure: false, // true for 465, false for other ports
+                        auth: {
+                                user: process.env.SMTP_USER, // generated ethereal user
+                                pass: process.env.SMTP_PASSWORD, // generated ethereal password
+                        },
+
+                });
+
+
+                // from: '"Flaq Academy" <welcome@flaq.club>',
+                //
+                //
+                let info = await transporter.sendMail({
+                        from: '"Flaq Academy👻" <welcome@flaq.club>', // sender address
+                        to: "ashwin@onpar.in", // list of receivers
+                        subject: fields["Subject"] as string, // Subject line
+                        html: email, // html body
+                });
+                console.log(info)
+
+
+
+                res.status(200).json({ seb: "Danke Dear" });
+
+
+        }
+
+        catch (e) {
+                console.log(e)
+                res.status(200).json({ name: 'Oops Doe' })
+        }
 
 }
